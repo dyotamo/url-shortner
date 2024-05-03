ee895b0bb8fe4fdd0ebb6255f7efc727a3db495b
Dassone J. Yotamo <dassone.jotamo@vm.co.mz>
2024-05-03 13:43:16 +0200

    refactor: organize files.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,10 @@
-use rocket::{http::Status, response::status, Request};
-
 #[macro_use]
 extern crate rocket;
 
-#[post("/", data = "<url>")]
-pub fn shortten(url: &str) -> Result<String, Status> {
-    short::set_url(url).map_err(|_| Status::BadRequest)
-}
+mod web;
 
-#[get("/<short>")]
-pub fn get_short(short: &str) -> Option<String> {
-    short::get_url(short)
-}
-
-#[catch(404)]
-pub fn not_found<'a>(_: &'a Request) -> status::NotFound<&'a str> {
-    status::NotFound("oops, not found")
-}
-
-#[catch(405)]
-pub fn method_not_allowed<'a>(_: &'a Request) -> status::Custom<&'a str> {
-    status::Custom(Status::MethodNotAllowed, "oops, method not allowed")
-}
-
-#[catch(500)]
-pub fn server_error<'a>(_: &'a Request) -> status::Custom<&'a str> {
-    status::Custom(Status::InternalServerError, "oops, internal server error")
-}
+use web::util::{method_not_allowed, not_found, server_error};
+use web::{get_short, shortten};
 
 #[launch]
 fn rocket() -> _ {
